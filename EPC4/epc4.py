1a6c1393c83caa279e6fb2e21e27e362336b714b
Guilherme Rocha <grgoncal@gmail.com>
2019-04-15 00:32:53 -0300

    EPC4 Functional ?

--- EPC4/epc4.py
+++ EPC4/epc4.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 #////////////////////////////////////////////////////////////////////
 # CONSTANTS /////////////////////////////////////////////////////////
@@ -18,12 +19,15 @@ numberNeurons2ndLayer = 1
 # TRAIN /////////////////////////////////////////////////////////////
 #////////////////////////////////////////////////////////////////////
 
-def train(x, d, w1, w2):
+def train(x, d, w1, w2, testNumber):
     # INITIALIZE EPOCHS AND ERR -------------------------------------
     epoch = 0
     Eqm = 1
     lEqm = 0
 
+    # CHART LIST ---------------------------------------------------
+    chart = [[],[]]
+    
     while abs(Eqm - lEqm) > errTol and epoch < maxEpochs:
         lEqm = Eqm
         Eqm = 0
@@ -37,19 +41,19 @@ def train(x, d, w1, w2):
             Y2 = I2
             D1 = pd.DataFrame(np.zeros((numberNeurons1stLayer, 1)))
             D2 = pd.DataFrame(np.zeros((numberNeurons2ndLayer, 1)))
-            
+
             # FORWARD
             for neuronIndex1stLayer in range(numberNeurons1stLayer):
                 I1.iloc[neuronIndex1stLayer] += (w1.iloc[:,neuronIndex1stLayer] * x.iloc[inputIndex,:]).sum()
                 Y1.iloc[neuronIndex1stLayer] = logistic(I1.iloc[neuronIndex1stLayer])
-
+            
             for name in reversed(list(Y1.index)):
                 Y1.rename(index = {name : name + 1}, inplace = True)
             Y1.loc[0] = -1
             Y1 = Y1.sort_index()
-            
+
             for neuronIndex2ndLayer in range(numberNeurons2ndLayer):
-                I2.iloc[neuronIndex2ndLayer] += (Y1.iloc[:,0] * w2.iloc[:,0]).sum()
+                I2.iloc[neuronIndex2ndLayer] += (Y1.iloc[:,neuronIndex2ndLayer] * w2.iloc[:,neuronIndex2ndLayer]).sum()
                 Y2.iloc[neuronIndex2ndLayer] = logistic(I2.iloc[neuronIndex2ndLayer])
 
             # BACKWARD
@@ -58,11 +62,11 @@ def train(x, d, w1, w2):
                 for wIndex2ndLayer in range(w2.shape[0]):
                     w2.iloc[wIndex2ndLayer, neuronIndex2ndLayer] += learningRate * D2.iloc[neuronIndex2ndLayer, 0] * Y1.iloc[wIndex2ndLayer, 0]
 
-            for neuronIndex1stLayer in range(numberNeurons1stLayer):
-                for neuronIndex2ndLayer in range(numberNeurons2ndLayer):
-                    D1.iloc[neuronIndex1stLayer] = (D2.iloc[neuronIndex2ndLayer] * w2.iloc[neuronIndex1stLayer]).sum() * logistic(I1.iloc[neuronIndex1stLayer]) * (1 - logistic(I1.iloc[neuronIndex1stLayer])) 
-                for inputIndex1stLayer in range(x.shape[1]):
-                    w1.iloc[inputIndex1stLayer, neuronIndex1stLayer] += learningRate * D1.iloc[neuronIndex1stLayer, 0] * x.iloc[inputIndex, inputIndex1stLayer]
+            for neuronIndex2ndLayer in range(numberNeurons2ndLayer): # Neuronio 0 de saida
+                for neuronIndex1stLayer in range(numberNeurons1stLayer): # 0 a 9 neuronios de entrada
+                    D1.iloc[neuronIndex1stLayer] = D2.iloc[neuronIndex2ndLayer] * w2.iloc[neuronIndex1stLayer, neuronIndex2ndLayer] * logistic(I1.iloc[neuronIndex1stLayer]) * (1 - logistic(I1.iloc[neuronIndex1stLayer]))
+                    for inputIndex1stLayer in range(x.shape[1]):
+                        w1.iloc[inputIndex1stLayer, neuronIndex1stLayer] += learningRate * D1.iloc[neuronIndex1stLayer, 0] * x.iloc[inputIndex, inputIndex1stLayer]
 
         # REPEAT FORWARD STEP
         for inputIndex in range(x.shape[0]):
@@ -87,11 +91,19 @@ def train(x, d, w1, w2):
                 Eqm += 0.5 * (d.iloc[inputIndex] - Y2.iloc[neuronIndex2ndLayer ,0]) * (d.iloc[inputIndex] - Y2.iloc[neuronIndex2ndLayer ,0])
             
         # CALCULATE NEW ERROR
-        Eqm = (1/(x.shape[0] * 2)) * Eqm
+        Eqm = (Eqm/x.shape[0])
         print Eqm
-
+        
+        chart[0].append(epoch)
+        chart[1].append(Eqm)
+        
         # INCREMENT EPOCH
         epoch += 1
+
+    plt.figure(1)
+    plt.plot(chart[0], chart[1])
+    plt.savefig("./" + str(testNumber) + ".png", dpi = 500)
+    plt.close()
 
     return w1, w2
 
@@ -99,8 +111,37 @@ def train(x, d, w1, w2):
 # TEST //////////////////////////////////////////////////////////////
 #////////////////////////////////////////////////////////////////////
 
-def test(w):
-    return 0
+def test(w1, w2):
+    x = pd.read_csv('./test.csv', header = None)
+    for name in reversed(x.columns.values):
+        x.rename(columns = {name : name + 1}, inplace = True)
+    x.insert(loc = 0, column=0, value = np.full((x.shape[0],1), -1))
+    
+    y = []
+
+    # REPEAT FORWARD STEP
+    for inputIndex in range(x.shape[0]):
+            
+        I1 = pd.DataFrame(np.zeros((numberNeurons1stLayer,1)))
+        I2 = pd.DataFrame(np.zeros((numberNeurons2ndLayer,1)))
+        Y1 = I1
+        Y2 = I2
+
+        for neuronIndex1stLayer in range(numberNeurons1stLayer):
+            I1.iloc[neuronIndex1stLayer] += (w1.iloc[:,neuronIndex1stLayer] * x.iloc[inputIndex,:]).sum()
+            Y1.iloc[neuronIndex1stLayer] = logistic(I1.iloc[neuronIndex1stLayer])
+
+        for name in reversed(list(Y1.index)):
+            Y1.rename(index = {name : name + 1}, inplace = True)
+        Y1.loc[0] = -1
+        Y1 = Y1.sort_index()
+                
+        for neuronIndex2ndLayer in range(numberNeurons2ndLayer):
+            I2.iloc[neuronIndex2ndLayer] += (Y1.iloc[:,0] * w2.iloc[:,0]).sum()
+            Y2.iloc[neuronIndex2ndLayer] = logistic(I2.iloc[neuronIndex2ndLayer])
+            y.append(Y2.iloc[neuronIndex2ndLayer,0])
+
+    return y
 
 #////////////////////////////////////////////////////////////////////
 # LOGISTIC FUNCTION /////////////////////////////////////////////////
@@ -127,6 +168,6 @@ for i in range(1, trainNumber + 1):
     w2 = pd.DataFrame(np.random.rand(numberNeurons1stLayer + 1, numberNeurons2ndLayer))
 
     print("[TRAINING NUMBER " + str(i) + "]")
-    w1, w2 = train(x, d, w1, w2)
-    # y = test()
-    # print("[RESULT OF TRAINING NUMBER " + str(i) + "] " + str(y) + "\n")
+    w1, w2 = train(x, d, w1, w2, i)
+    y = test(w1, w2)
+    print("[RESULT OF TRAINING NUMBER " + str(i) + "] " + str(y) + "\n")
